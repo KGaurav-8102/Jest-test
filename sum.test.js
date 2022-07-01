ed6f77cada6f607a8e7ed37f2d406b1d37ed3feb
Gaurav Yadav <gk5480325@gmail.com>
2022-07-01 14:19:03 +0530

    Testing Truthiness

--- sum.test.js
+++ sum.test.js
@@ -13,3 +13,20 @@ test('object assignment', () => {
     data['two'] = 2;
     expect(data).toEqual({one: 1, two: 2});
   });
+
+test('adding positive number is not zero', () => {
+    for (let a = 1; a < 10; a++) {
+        for (let b = 1; b < 10; b++) {
+          expect(a + b).not.toBe(0);
+        }
+      }
+});
+
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+})

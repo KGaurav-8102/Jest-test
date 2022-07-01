987310ec8aa0deb6fb5eaf5db9e512a20a8a390f
Gaurav Yadav <gk5480325@gmail.com>
2022-07-01 14:30:17 +0530

    Floating Number is Tested

--- sum.test.js
+++ sum.test.js
@@ -50,3 +50,8 @@ test('two plus two', () => {
     expect(value).toBe(4);
     expect(value).toEqual(4);
 }) 
+
+test('adding floating to point numbers', () => {
+    const value = 0.1 + 0.2;
+    expect(value).toBeCloseTo(0.3);
+}) 

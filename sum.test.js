7a0477a51c176a15d6168e0215f51330ffe82c87
Gaurav Yadav <gk5480325@gmail.com>
2022-07-01 14:35:04 +0530

    String Test is Done

--- sum.test.js
+++ sum.test.js
@@ -55,3 +55,7 @@ test('adding floating to point numbers', () => {
     const value = 0.1 + 0.2;
     expect(value).toBeCloseTo(0.3);
 }) 
+
+test('there is no I in the Team', () => {
+    expect('Team').not.toMatch(/I/);
+})

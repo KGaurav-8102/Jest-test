7c429d8898e6ef7098ca6e9ee65130e513cb0ced
Gaurav Yadav <gk5480325@gmail.com>
2022-07-01 14:22:10 +0530

    Second Truthiness Test

--- sum.test.js
+++ sum.test.js
@@ -30,3 +30,12 @@ test('null', () => {
     expect(n).not.toBeTruthy();
     expect(n).toBeFalsy();
 })
+
+test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+})
